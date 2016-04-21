2e17aa5b7148e95b5e988028c93be154385d4105
jeroes <jesus.rodriguez@ibadia.cat>
2016-04-21 07:15:13 -0700

    Primer Commit. Añadido en la clase main una nueva variable de tipo Integer.

--- DemoJava/src/demojava/DemoJava.java
+++ DemoJava/src/demojava/DemoJava.java
@@ -15,7 +15,7 @@ public class DemoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Integer variableDePruebParaElCommit;
     }
     
 }
